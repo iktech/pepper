61106e436befc68931a569add85d859fdede3499
Igor Kolomiyets <ikolomiyets@gmail.com>
2022-10-23 16:58:50 +0100

    Adding function isset to validate in template that specific model property is set

--- model/model.go
+++ model/model.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"io/fs"
 	"log"
+	"reflect"
 )
 
 type Model struct {
@@ -31,6 +32,17 @@ func (m Model) IsActive(path string) string {
 	return "link"
 }
 
+func isset(name string, data interface{}) bool {
+	v := reflect.ValueOf(data)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		return false
+	}
+	return v.FieldByName(name).IsValid()
+}
+
 func (m Model) Render(Debug bool, data interface{}) (int, string, string, *bytes.Buffer, *ProcessingError) {
 	if Debug {
 		m.Logger.Printf("using %s template", m.Template)
@@ -39,7 +51,7 @@ func (m Model) Render(Debug bool, data interface{}) (int, string, string, *bytes
 	patterns := []string{m.Template}
 	patterns = append(patterns, m.Includes...)
 
-	t, err := template.New(m.Template).ParseFS(m.TemplatesDirectory, patterns...)
+	t, err := template.New(m.Template).Funcs(template.FuncMap{"isset": isset}).ParseFS(m.TemplatesDirectory, patterns...)
 	if err != nil {
 		m.Logger.Println(err)
 		return 0, "", "", nil, &ProcessingError{ResponseCode: 500}
